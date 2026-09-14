f8893bdba5d8fc93ca2a733e902003bbfcd76be5
LeoAmby <leoamby@gmail.com>
2026-09-14 16:36:01 +0300

    child routerlink

--- src/app/pages/employee-list/employee-list.ts
+++ src/app/pages/employee-list/employee-list.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-employee-list',
   styleUrl: './employee-list.css',
   templateUrl: './employee-list.html',
